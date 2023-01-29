79cb84781119c9b92c4e348274e283e440f5db95
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-01-29 17:10:37 -0300

    feat: routes

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,25 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import { DAY_SIZE, HabitDay } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const dateFromYearStart = generateRangeDatesFromYearStart()
+const minimuSummaryDateSizes = 18 * 5
+const amountOfDaysToFill = minimuSummaryDateSizes - dateFromYearStart.length
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16 ">
       <Header />
-      <View className='flex-row mt-6 mb-2'>
+      <View className="flex-row mt-6 mb-2">
         {weekDays.map((weekDay, i) => (
           <Text
             key={`${weekDay}-${i}`}
             className="text-zinc-400 text-xl font-bold text-center mx-1"
-            style={{width: DAY_SIZE}}
+            style={{ width: DAY_SIZE }}
           >
             {weekDay}
           </Text>
         ))}
       </View>
-      <HabitDay />
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100}}>
+        <View className="flex-row flex-wrap">
+          {dateFromYearStart.map((date) => (
+            <HabitDay key={date.toString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -65,7 +65,84 @@ export async function appRoute(app: FastifyInstance) {
     })
     return {
       possibleHabits,
-      compliteHabits
+      compliteHabits,
     }
+  })
+
+  //completar / não-complementar um hábito
+  app.patch('/habits/:id/toggle', async (resquest) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(resquest.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    })
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    })
+
+    if (dayHabit) {
+      // remove marcação de completo
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      })
+    } else {
+      //Completar o habito nesse dia
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      })
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+
+    return summary
   })
 }
